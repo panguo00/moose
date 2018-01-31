6213ff3cc972be4d3c60d058a1472f224d9ec543
Benjamin Spencer <benjamin.spencer@inl.gov>
2018-01-30 17:34:47 -0700

    Add interaction integral vpp

---
+++ modules/tensor_mechanics/include/vectorpostprocessors/InteractionIntegralVPP.h
@@ -0,0 +1,101 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#ifndef INTERACTIONINTEGRALVPP_H
+#define INTERACTIONINTEGRALVPP_H
+
+#include "ElementVectorPostprocessor.h"
+#include "CrackFrontDefinition.h"
+
+// Forward Declarations
+class InteractionIntegralVPP;
+class RankTwoTensor;
+
+template <>
+InputParameters validParams<InteractionIntegralVPP>();
+
+/**
+ * This vectorpostprocessor computes the Interaction Integral
+ *
+ */
+class InteractionIntegralVPP : public ElementVectorPostprocessor
+{
+public:
+  InteractionIntegralVPP(const InputParameters & parameters);
+
+  virtual void initialSetup() override;
+  virtual void initialize() override;
+  virtual void execute() override;
+  virtual void finalize() override;
+  virtual void threadJoin(const UserObject & y) override;
+
+  static MooseEnum qFunctionType();
+  static MooseEnum sifModeType();
+
+protected:
+  Real computeQpIntegral(const unsigned int crack_front_point_index, const Real scalar_q, const RealVectorValue & grad_of_scalar_q);
+  void computeAuxFields(RankTwoTensor & aux_stress, RankTwoTensor & grad_disp);
+  void computeTFields(RankTwoTensor & aux_stress, RankTwoTensor & grad_disp);
+  unsigned int _ndisp;
+  const CrackFrontDefinition * const _crack_front_definition;
+  bool _has_crack_front_point_index;
+  const unsigned int _crack_front_point_index;
+  bool _treat_as_2d;
+  const MaterialProperty<RankTwoTensor> * _stress;
+  const MaterialProperty<RankTwoTensor> * _strain;
+  std::vector<const VariableGradient *> _grad_disp;
+  const bool _has_temp;
+  const VariableGradient & _grad_temp;
+  Real _K_factor;
+  bool _has_symmetry_plane;
+  Real _poissons_ratio;
+  Real _youngs_modulus;
+  unsigned int _ring_index;
+  const MaterialProperty<RankTwoTensor> * _total_deigenstrain_dT;
+  std::vector<Real> _q_curr_elem;
+  const std::vector<std::vector<Real>> * _phi_curr_elem;
+  const std::vector<std::vector<RealGradient>> * _dphi_curr_elem;
+  Real _kappa;
+  Real _shear_modulus;
+  Real _r;
+  Real _theta;
+  unsigned int _qp;
+
+  enum class QMethod
+  {
+    Geometry,
+    Topology
+  };
+
+  const QMethod _q_function_type;
+
+  enum class PositionType
+  {
+    Angle,
+    Distance
+  };
+
+  const PositionType _position_type;
+
+  enum class SifMethod
+  {
+    KI,
+    KII,
+    KIII,
+    T
+  };
+
+  const SifMethod _sif_mode;
+
+  VectorPostprocessorValue & _x;
+  VectorPostprocessorValue & _y;
+  VectorPostprocessorValue & _z;
+  VectorPostprocessorValue & _position;
+  VectorPostprocessorValue & _interaction_integral;
+
+};
+
+#endif // INTERACTIONINTEGRALVPP_H
